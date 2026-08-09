a80edd37765b2e70c3bccb2c2fb6d51f438b3630
lifer_x <86353560+lifer-x@users.noreply.github.com>
2026-08-09 21:21:56 +0700

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-n,m=map(int,input().split())
-fl=[]
-a,b=0,1
-while b<=m:
-    if b>=n:fl.append(b)
-    a,b=b,a+b
-if len(fl)>0:
-    print(*fl)
-else:print("В заданном диапазоне нет чисел Фибоначчи")
